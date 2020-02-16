692afd0791176acfe0df310557deedbedd462d04
Rohan Viswanathan <rohanvis16@gmail.com>
2020-02-16 14:30:45 -0800

    Initial commit.

---
+++ opencv-vision/constants.py
@@ -0,0 +1,9 @@
+# Constants for FRC 2020 Vision
+
+FRAME_WIDTH = 600 #in pixels (for resizing frame)
+MIN_CIRCLE_RADIUS = 10 #in pixels
+
+LOWER_YELLOW = (11, 24, 61) #in HSV color space
+UPPER_YELLOW = (47, 255, 217) #in HSV color space
+
+FIELD_OF_VIEW_X = 78 #degrees
